d6a64cea22a4abef3bdc10f536ba61fc23640288
eric <eric.thomas@formation-industries-fc.fr>
2022-01-18 14:22:45 +0100

    q1,q2,q3

--- main.c
+++ main.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 #include <stdbool.h>
+int *ptrint;
+int choix=5;
 /********comparer le choix fait par l'utilisateur à la variable choix par indirection********************/
 /*****************************si identique renvoie true si différent renvoie false***********************/
-bool choix(int choixutilisateur);
+bool choixvaleur(int choixutilisateur);
 
-bool choix(int choixutilisateur) {
+bool choixvaleur(int choixutilisateur) {
 
-    return 0;
+if(choixutilisateur==*ptrint)
+{
+    return true;
+}
+else
+    return false;
+
 }
 
-bool chxoix2(int *choix2utilisateur) {
-    return 0;
+bool choixreference(int *choix2utilisateur) {
+    if(*choix2utilisateur==*ptrint)
+    {
+        return true;
+    }
+    else
+        return false;
 }
 
-bool chxoix2(int* choix2utilisateur);
+bool choixreference(int* choix2utilisateur);
 
 
 int main() {
     /**********Déclaration d'un pointeur de type entier ptrint et entier choix=5**********/
-    int *ptrint;
-    int choix=5;
+
     /**********Valoriser ptrint avec l'adresse mémoire de choix***************************/
     ptrint=&choix;
     /**********écrire le code des fonctions**********************************************/
-
+    printf("%p\n",ptrint);
+    printf("%p\n",&choix);
     /*********faire le test avec une saisie utilisateur***********************************/
 
     return 0;
