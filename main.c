275f3cfd77912c1c9c04290e9861104d64c3eec8
eric <eric.thomas@formation-industries-fc.fr>
2022-01-18 14:09:03 +0100

    q1 et q2

--- main.c
+++ main.c
@@ -2,14 +2,26 @@
 #include <stdbool.h>
 /********comparer le choix fait par l'utilisateur à la variable choix par indirection********************/
 /*****************************si identique renvoie true si différent renvoie false***********************/
-bool choix(int choix);
-bool choix2(int* choix2);
+bool choix(int choixutilisateur);
+
+bool choix(int choixutilisateur) {
+
+    return 0;
+}
+
+bool chxoix2(int *choix2utilisateur) {
+    return 0;
+}
+
+bool chxoix2(int* choix2utilisateur);
+
 
 int main() {
     /**********Déclaration d'un pointeur de type entier ptrint et entier choix=5**********/
-
+    int *ptrint;
+    int choix=5;
     /**********Valoriser ptrint avec l'adresse mémoire de choix***************************/
-
+    ptrint=&choix;
     /**********écrire le code des fonctions**********************************************/
 
     /*********faire le test avec une saisie utilisateur***********************************/
