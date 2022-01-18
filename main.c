249731b242a40a58caa10c213debd6b54e4d27e9
eric <eric.thomas@formation-industries-fc.fr>
2022-01-18 13:43:29 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+/********comparer le choix fait par l'utilisateur à la variable choix par indirection********************/
+/*****************************si identique renvoie true si différent renvoie false***********************/
+bool choix(int choix);
+bool choix2(int* choix2);
+
+int main() {
+    /**********Déclaration d'un pointeur de type entier ptrint et entier choix=5**********/
+
+    /**********Valoriser ptrint avec l'adresse mémoire de choix***************************/
+
+    /**********écrire le code des fonctions**********************************************/
+
+    /*********faire le test avec une saisie utilisateur***********************************/
+
+    return 0;
+}
